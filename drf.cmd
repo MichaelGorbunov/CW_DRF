a8b65c345f7f17ddf298e1fbccb0528ed377c236
Michael Gorbunov <mg0rby@yandex.ru>
2024-12-16 17:24:23 +0300

    init project

---
+++ drf.cmd
@@ -0,0 +1,17 @@
+
+poetry add --group dev python-dotenv
+poetry add --group dev psycopg2
+poetry add --group dev pillow
+poetry add --group dev django
+poetry add --group dev python-dotenv
+poetry add --group dev djangorestframework
+poetry add --group dev django-filter
+poetry add --group dev djangorestframework-simplejwt
+poetry add --group task celery
+poetry add --group task django-celery-beat
+poetry add --group docs drf-yasg
+poetry add --group test coverage
+
+
+
+
