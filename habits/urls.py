8a296746d099971a40a4bd942c40f1be8cf50712
Michael Gorbunov <mg0rby@yandex.ru>
2024-12-17 18:29:25 +0300

    crud для привычек

--- habits/urls.py
+++ habits/urls.py
@@ -1,12 +1,22 @@
 from django.urls import path
 from habits.apps import HabitsConfig
-from habits.views import (HabitListAPIView,HabitCreateAPIView)
+from habits.views import (HabitListAPIView, HabitRetrieveAPIView,
+                          HabitCreateAPIView, HabitUpdateAPIView,
+                          HabitDestroyAPIView, HabitPublishedListAPIView)
 
 app_name = HabitsConfig.name
 
 urlpatterns = [
     # вывод списка привычек
     path("list/", HabitListAPIView.as_view(), name="habits-list"),
-# создание привычки
+    # вывод списка публичных привычек
+    path("published/", HabitPublishedListAPIView.as_view(), name="habits-published"),
+    # просмотр одной привычки
+    path("<int:pk>/detail/", HabitRetrieveAPIView.as_view(), name="habits-retrieve"),
+    # создание привычки
     path("create/", HabitCreateAPIView.as_view(), name="habits-create"),
-   ]
+    # изменение одной привычки
+    path("<int:pk>/update/", HabitUpdateAPIView.as_view(), name="habits-update"),
+    # удаление одной привычки
+    path("<int:pk>/delete/", HabitDestroyAPIView.as_view(), name="habits-delete" ),
+]
--- habits/views.py
+++ habits/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, DestroyAPIView, UpdateAPIView, RetrieveAPIView
 
 from habits.models import Habit
+from habits.permissions import IsOwner
 from habits.serializers import HabitSerializer
 
 
@@ -9,6 +10,7 @@ class HabitListAPIView(ListAPIView):
     """Контроллер вывода списка привычек"""
     queryset = Habit.objects.all()
     serializer_class = HabitSerializer
+
 
 class HabitCreateAPIView(CreateAPIView):
     """Контроллер создания новой привычки"""
@@ -20,3 +22,30 @@ class HabitCreateAPIView(CreateAPIView):
         habit = serializer.save()
         habit.owner = self.request.user
         habit.save()
+
+
+class HabitPublishedListAPIView(ListAPIView):
+    """Контроллер вывода списка публичных привычек"""
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.filter(is_published=True)
+
+
+class HabitRetrieveAPIView(RetrieveAPIView):
+    """Контроллер просмотра одной привычки"""
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = (IsOwner,)
+
+
+class HabitUpdateAPIView(UpdateAPIView):
+    """Контроллер изменения одной привычки"""
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = (IsOwner,)
+
+
+class HabitDestroyAPIView(DestroyAPIView):
+    """Контроллер удаления одной привычки"""
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = (IsOwner,)
