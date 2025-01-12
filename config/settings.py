efc69ef00e76c9c8c364d081a7ca5310e8eb8d1b
Michael Gorbunov <mg0rby@yandex.ru>
2025-01-12 12:04:50 +0300

    ALLOWED_HOSTS SERVER_IP in .env

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['s120601.foxcdn.net']
+SERVER_IP = os.getenv("SERVER_IP")
+ALLOWED_HOSTS = [SERVER_IP]
 
 # Application definition
 
